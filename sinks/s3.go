b4975ca12967b57dd8fb77b886d410131261ec39
Benjami Luolajan-Mikkola <benjami.luolajan-mikkola@solita.fi>
2025-08-08 17:31:23 +0300

    Remove support for streaming to S3, it is broken

--- sinks/s3.go
+++ sinks/s3.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -15,11 +14,10 @@ import (
 )
 
 type S3Sink struct {
-	context       context.Context
-	client        *s3.Client
-	bucket        string
-	prefix        string
-	forceSeekable bool
+	context context.Context
+	client  *s3.Client
+	bucket  string
+	prefix  string
 }
 
 func (s *S3Sink) StoreMessage(msg core.Message) error {
@@ -42,15 +40,13 @@ func (s *S3Sink) StoreMessage(msg core.Message) error {
 
 func (s *S3Sink) StoreAttachment(id string, data io.Reader) error {
 	key := s.prefix + "/attachments/" + id
-	if s.forceSeekable {
-		// If S3 endpoint uses HTTP, we need our data to be a seekable reader
-		// for checksum calculations; otherwise, this should be avoided to save memory
-		var buf bytes.Buffer
-		if _, err := io.Copy(&buf, data); err != nil {
-			return fmt.Errorf("failed to read attachment data: %w", err)
-		}
-		data = bytes.NewReader(buf.Bytes())
+	// We don't want to trust sender to provide valid attachment size, so read it fully
+	// and then upload to S3
+	var buf bytes.Buffer
+	if _, err := io.Copy(&buf, data); err != nil {
+		return fmt.Errorf("failed to read attachment data: %w", err)
 	}
+	data = bytes.NewReader(buf.Bytes())
 	_, err := s.client.PutObject(s.context, &s3.PutObjectInput{
 		Bucket: &s.bucket,
 		Key:    &key,
@@ -71,21 +67,16 @@ func NewS3(bucket, prefix, baseEndpoint string) (*S3Sink, error) {
 		return nil, err
 	}
 
-	forceSeekable := false
 	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
 		if baseEndpoint != "" {
 			o.BaseEndpoint = aws.String(baseEndpoint)
 			o.UsePathStyle = true
-			if strings.HasPrefix(baseEndpoint, "http://") {
-				forceSeekable = true
-			}
 		}
 	})
 	return &S3Sink{
-		context:       context.TODO(),
-		client:        client,
-		bucket:        bucket,
-		prefix:        prefix,
-		forceSeekable: forceSeekable,
+		context: context.TODO(),
+		client:  client,
+		bucket:  bucket,
+		prefix:  prefix,
 	}, nil
 }
